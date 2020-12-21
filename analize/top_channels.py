fa4fb5e55ece6d239035fb887de9416bcdbc6912
ik6cgsg <kozlovilya@bk.ru>
2020-12-21 21:32:38 +0300

    Add top channels

---
+++ analize/top_channels.py
@@ -0,0 +1,14 @@
+from pyspark.sql.types import StringType
+from analize.utils import *
+
+spark = get_session()
+df = spark.read.format("mongo") \
+    .option("uri", "mongodb://127.0.0.1/youtube.gb_videos") \
+    .load()
+
+cd = df.groupBy(["channel_title"]).count().sort("count", ascending=False)
+cd = cd.withColumnRenamed("count", "trend_days")
+days = df.groupBy("date").count().count()
+cd = cd.withColumn("trendable", cd.trend_days / days).limit(15)
+cd.show(truncate=False)
+save_collection(cd, "top_channels")
